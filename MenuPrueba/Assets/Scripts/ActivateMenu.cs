bc293111f2ed93ca7c2bc23094e8f132fc8dd986
dvega03 <dvega03@users.noreply.github.com>
2018-04-17 18:38:10 +0200

    Añadido la funcionalidad del menu en una escena.

---
+++ MenuPrueba/Assets/Scripts/ActivateMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateMenu : MonoBehaviour {
+
+    public GameObject menu;
+    public bool Active = false;
+
+    private void Start()
+    {
+        menu.SetActive(Active);
+        
+    }
+
+    void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            changeCanvasProperty(menu, Active);
+            Active = !Active;
+        }
+        
+    }
+
+    void changeCanvasProperty(GameObject yourCanvas, bool disable)
+    {
+        if (disable)
+            yourCanvas.SetActive(false);
+        else
+            yourCanvas.SetActive(true);
+    }
+}
